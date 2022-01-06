c26d9bc2dbc48bc8a3a2f98c2d9c344464496dff
Josecontis <peppeco98@gmail.com>
2022-01-06 22:19:57 +0100

    Add SearchField component

---
+++ src/Components/SearchField/SearchField.js
@@ -0,0 +1,10 @@
+import "./SearchField.css";
+
+export const SearchField = () => {
+  return (
+    <>
+      <input placeholder="Ricerca piatti, caffé, pizze, etc..." type="text" />
+      <i class="fa fa-search"></i>
+    </>
+  );
+};
